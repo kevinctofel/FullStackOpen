a0a13eaa35708e9c78f365415d8cb85b094c9944
Kevin C. Tofel <kevin@kctofel.com>
2023-08-21 16:40:03 -0400

    Exercise 1-7 completed

--- Part1/CourseInfo/Unicafe/unicafe/src/App.js
+++ Part1/CourseInfo/Unicafe/unicafe/src/App.js
@@ -5,17 +5,24 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setAll(all + 1);
+    setTotal(total + 1);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setAll(all + 1);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setAll(all + 1);
+    setTotal(total - 1);
   };
 
   return (
@@ -24,10 +31,13 @@ const App = () => {
       <button onClick={handleGoodClick}>Good</button>
       <button onClick={handleNeutralClick}>Neutral</button>
       <button onClick={handleBadClick}>Bad</button>
-      <h2>Statistics</h2>
+      {/* <h2>Statistics</h2> */}
       <div>Good: {good}</div>
       <div>Neutral: {neutral}</div>
       <div>Bad: {bad}</div>
+      <div>All: {all}</div>
+      <div>Average: {(good - bad) / all}</div> {/* not working*/}
+      <div>Positive: {good / all}%</div>
     </div>
   );
 };
