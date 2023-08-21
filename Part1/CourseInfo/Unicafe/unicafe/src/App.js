eb756c8dd96be18c2b4e352887f93d03bff326bc
Kevin C. Tofel <kevin@kctofel.com>
2023-08-21 16:59:31 -0400

    Started Exercise 1-8

--- Part1/CourseInfo/Unicafe/unicafe/src/App.js
+++ Part1/CourseInfo/Unicafe/unicafe/src/App.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  console.log(props);
+  return <div>Good stats: {props.goodTotal}</div>;
+};
+
 const App = () => {
-  // save clicks of each button to its own state
+  // save clicks of each button to its own state OR place them all in an object
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -31,8 +36,9 @@ const App = () => {
       <button onClick={handleGoodClick}>Good</button>
       <button onClick={handleNeutralClick}>Neutral</button>
       <button onClick={handleBadClick}>Bad</button>
-      {/* <h2>Statistics</h2> */}
+      <h2>Statistics</h2>
       <div>Good: {good}</div>
+      <Statistics goodTotal={good} />
       <div>Neutral: {neutral}</div>
       <div>Bad: {bad}</div>
       <div>All: {all}</div>
