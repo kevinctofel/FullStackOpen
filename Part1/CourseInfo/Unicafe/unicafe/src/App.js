8386c2d6e221ba296b7e7ecf282ff2360987abe3
Kevin C. Tofel <kevin@kctofel.com>
2023-08-21 15:56:09 -0400

    Excercise 1-6 complete

---
+++ Part1/CourseInfo/Unicafe/unicafe/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+      <h2>Statistics</h2>
+      <div>Good: {good}</div>
+      <div>Neutral: {neutral}</div>
+      <div>Bad: {bad}</div>
+    </div>
+  );
+};
+
+export default App;
