d7c6baef1d9456cab607431ef4337fd367628e48
Kevin C. Tofel <kevin@kctofel.com>
2023-09-01 11:01:53 -0400

    Updated statistics

--- Part1/CourseInfo/Unicafe/unicafe/src/App.js
+++ Part1/CourseInfo/Unicafe/unicafe/src/App.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+const Button = (props) => {
+  // Reusable Button component
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
 const Statistics = (props) => {
   if (props.stats["all"] === 0) return <p>No feedback given yet.</p>;
 
@@ -23,12 +28,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [all, setAll] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [score, setScore] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
     setAll(all + 1);
-    setTotal(total + 1);
+    setScore(score + 1);
   };
 
   const handleNeutralClick = () => {
@@ -39,18 +44,25 @@ const App = () => {
   const handleBadClick = () => {
     setBad(bad + 1);
     setAll(all + 1);
-    setTotal(total - 1);
+    setScore(score - 1);
   };
 
   return (
     <div>
       <h2>Give Feedback</h2>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button>
-      <button onClick={handleBadClick}>Bad</button>
+      {/* Pass label and onClick function to Button component */}
+      <Button handleClick={() => handleGoodClick()} text="Good" />
+      <Button handleClick={() => handleNeutralClick()} text="Neutral" />
+      <Button handleClick={() => handleBadClick()} text="Bad" />
       <h2>Statistics</h2>
       <Statistics
-        stats={{ good: good, neutral: neutral, bad: bad, all: all }} // object of key-value pairs for states
+        stats={{
+          good: good,
+          neutral: neutral,
+          bad: bad,
+          all: all,
+          score: score,
+        }} // object of key-value pairs for states
       />
     </div>
   );
