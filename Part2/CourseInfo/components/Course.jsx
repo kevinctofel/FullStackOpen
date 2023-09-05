5db87827fe7969d179ffaba68731b75ba87f8280
Kevin C. Tofel <kevin@kctofel.com>
2023-09-05 18:26:49 -0400

    Added total exercises

--- Part2/CourseInfo/components/Course.jsx
+++ Part2/CourseInfo/components/Course.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-key */
 
 import Header from "./Header";
 
--- Part2/CourseInfo/components/Header.jsx
+++ Part2/CourseInfo/components/Header.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/prop-types */
 const Header = ({ parts }) => {
+  const initialValue = 0;
+
+  const total = parts.parts.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.exercises,
+    initialValue
+  );
+
+  console.log(parts.parts);
+
   return (
     <div>
-      {parts.parts.map((parts) => (
-        <p key={parts["id"]}>{parts["name"]}</p>
+      {parts.parts.map((part) => (
+        <p key={part["id"]}>
+          {part["name"]} {part["exercises"]}{" "}
+        </p>
       ))}
+
+      <h3>Total of {total} exercises</h3>
     </div>
   );
 };
