5b42844b5eafee474906d411e701f95857596ef0
Kevin C Tofel <kevin@kctofel.com>
2023-09-18 11:38:46 -0400

    Axios and db.json example

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,36 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import App from './App'
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+// import { useState, useEffect } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+// import axios from "axios";
+
+// axios.get("http://localhost:3001/notes").then((response) => {
+//   const notes = response.data;
+//   ReactDOM.createRoot(document.getElementById("root")).render(
+//     <App notes={notes} />
+//   );
+// });
+
+// const promise2 = axios.get("http://localhost:3001/foobar");
+// console.log(promise2);
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ];
